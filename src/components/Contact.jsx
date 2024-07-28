a8b32e191d3c4694d18ad7304f1adc1b609666c1
saifmn66 <patosaif33@gmail.com>
2024-07-28 11:03:32 +0100

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,43 +12,71 @@ export default function Contact() {
           className="absolute bottom-0 w-full sm:h-3/5 h-full bg-contain bg-center bg-no-repeat"
         ></div>
         <div className="relative max-w-screen z-10  md:grid-cols-2 grid ">
-            <div className="my-auto bg-[#3C3C3C] p-8 px-14 w-fit">
-                <h1>klhggilygyug</h1>
-                <h1>klhggilygyug</h1>
-                <h1>klhggilygyug</h1>
-                <h1>klhggilygyug</h1>
-                <h1>klhggilygyug</h1>
+            <div className="my-auto bg-[#3C3C3C] rounded-r-3xl p-8 px-14 w-fit">
+                <div className="flex p-2 text-white ">
+                  <div className="mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 max-w-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h1>cogebimmobiliere@gmail.com</h1>
+                  </div>
+                </div>
+                <div className="flex p-2 text-white ">
+                  <div className="mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 max-w-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h1>c+216 73 323 435</h1>
+                  </div>
+                </div>
+                <div className="flex p-2 text-white ">
+                  <div className="mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 max-w-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    </svg>
+
+                  </div>
+                  <div>
+                    <h1>Avenue de l'environnement, Sousse, <br/>
+                    Tunisia, 4000</h1>
+                  </div>
+                </div>
             </div>
-          <div className="pr-12 rounded-l-xl py-16 text-[#3B4862] bg-black/10 ">
+          <div className="md:pr-12 rounded-l-xl py-16 text-[#3B4862] bg-black/10 ">
             <form className="ml-auto sm:w-[40vw] space-y-4">
               <div  className="grid  gap-6 mb-6 md:grid-cols-2">
                 <div>
                   <label
                     
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    First name
+                    NOM ET PRENOM
                   </label>
                   <input
                     type="text"
                     id="first_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="John"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="NOM ET PRENOM"
                     required
                   />
                 </div>
                 <div>
                   <label
                     
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Last name
+                    EMAIL
                   </label>
                   <input
                     type="text"
                     id="last_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Doe"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="EMAIL"
                     required
                   />
                 </div>
@@ -57,30 +85,30 @@ export default function Contact() {
                 <div>
                   <label
                     
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Website URL
+                    TELEPHONE
                   </label>
                   <input
-                    type="url"
+                    type="text"
                     id="website"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="flowbite.com"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="TELEPHONE"
                     required
                   />
                 </div>
                 <div>
                   <label
                     
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Unique visitors (per month)
+                    PROJET
                   </label>
                   <input
-                    type="number"
+                    type="text"
                     id="visitors"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder=""
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="PROJET"
                     required
                   />
                 </div>
@@ -94,7 +122,7 @@ export default function Contact() {
                 type="button"
                 className="text-white bg-blue-500 hover:bg-blue-600 tracking-wide rounded-md text-sm px-4 py-3 w-full !mt-6"
               >
-                Send
+                ENVOYER
               </button>
             </form>
           </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,22 @@ import { Navbar } from "flowbite-react";
 export default function Nav() {
   return (
     <Navbar className="bg-white/80 fixed w-full mt-8 z-10" fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src={logo} className="mr-3 absolute h-20 p-4 bg-white sm:h-32" alt="Flowbite React Logo" />
+      <Navbar.Brand >
+        <img src={logo} className="mr-3 absolute h-20 p-4 bg-white sm:h-32" alt=" Logo" />
       </Navbar.Brand>
       
       <div className="block sm:py-3 justify-end">
       <Navbar.Toggle />
         <Navbar.Collapse>
-          <Navbar.Link href="#" active>
-            Home
+          <Navbar.Link className="text-[#1E1E1E]" href="#" active>
+          ACCUEIL
           </Navbar.Link>
-          <Navbar.Link href="#">
-            About
-          </Navbar.Link>
-          <Navbar.Link href="#">Services</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Contact</Navbar.Link>
+          <Navbar.Link className="text-[#1E1E1E]" href="#">A PROPOS</Navbar.Link>
+          <Navbar.Link className="text-[#1E1E1E]" href="#">CONTACT</Navbar.Link>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+          </svg>
+
         </Navbar.Collapse>
       </div>
     </Navbar>
